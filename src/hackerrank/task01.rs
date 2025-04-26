da3d978738c0571d0a1b114a321d69c8f2a3b85c
Daniil Kononenko <daniil.kononenko@e-u.edu.ua>
2025-04-26 20:22:42 +0300

    task1

--- src/hackerrank/task01.rs
+++ src/hackerrank/task01.rs
@@ -1,4 +1,26 @@
-#[test]
-fn test1() {
-    assert_eq!(1 + 1, 2);
+//Simple Array Sum
+
+use std::io;
+
+fn simple_array_sum(ar: Vec<i32>) -> i32 {
+    ar.iter().sum()
 }
+
+fn main() {
+    let mut input = String::new();
+    
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let _n: usize = input.trim().parse().expect("Expected a number");
+
+    input.clear();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let ar: Vec<i32> = input
+        .trim()
+        .split_whitespace()
+        .map(|x| x.parse().expect("Expected an integer"))
+        .collect();
+
+    let result = simple_array_sum(ar);
+    println!("{}", result);
+}
+
