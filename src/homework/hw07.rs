2ed406cc89511e33cfaf6f978d8adea00a595240
Danya Kononenko <116737236+hassagenok@users.noreply.github.com>
2025-05-25 09:48:18 +0300

    Update hw07.rs

---
+++ src/homework/hw07.rs
@@ -0,0 +1,30 @@
+fn invert_the_case(string: String) -> String {
+    let mut inverted_string = String::with_capacity(string.len());
+
+    for ch in string.chars() {
+        if ch.is_uppercase() {
+            inverted_string.extend(ch.to_lowercase());
+        } else if ch.is_lowercase() {
+            inverted_string.extend(ch.to_uppercase());
+        } else {
+            inverted_string.push(ch);
+        }
+    }
+
+    inverted_string
+}
+
+#[test]
+fn test() {
+    let data = [
+        ("Hello", "hELLO"),
+        ("Привіт", "пРИВІТ"),  
+        ("12345", "12345"),
+        ("TeСт", "тЕсТ"),
+    ];
+
+    for &(input, expected) in data.iter() {
+        assert_eq!(invert_the_case(input.to_string()), expected.to_string());
+        assert_eq!(invert_the_case(expected.to_string()), input.to_string());
+    }
+}
